57d5139b51e7397355851bcf516fa72a8d960217
BIGSosiska228 <andreymolofia@gmail.com>
2026-03-20 11:11:55 +0400

    Update circle.h

--- circle.h
+++ circle.h
@@ -1,33 +1,34 @@
 #pragma once
 #include "Shape.h"
+#include <iostream>
 
-class Circle : public Shape
-{
+class Circle : public Shape {
 private:
     unsigned int radius;
 
 public:
-    Circle(unsigned int x, unsigned int y, unsigned int r)
-        : Shape(x, y)
-    {
-        radius = r;
-    }
-
-    Circle(const Circle& source) : Shape(source)
-    {
-        radius = source.radius;
-    }
-
-    Shape* clone() override
-    {
+    Circle(unsigned int x, unsigned int y, unsigned int r) 
+        : Shape(x, y), radius(r) {}
+    
+    Circle(const Circle& source) 
+        : Shape(source), radius(source.radius) {}
+    
+    Shape* clone() const override {
         return new Circle(*this);
     }
-
-    void Draw() override
-    {
-        std::cout << "Circle "
-            << x << " "
-            << y << " "
-            << radius << std::endl;
+    
+    void Draw() const override {
+        std::cout << "Circle at (" << x << "," << y 
+                  << ") radius " << radius << std::endl;
     }
+    
+    void setSize(unsigned int size) override {
+        radius = size;
+    }
+    
+    unsigned int getSize() const override {
+        return radius;
+    }
+    
+    unsigned int getRadius() const { return radius; }
 };
