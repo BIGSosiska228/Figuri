7a60c1025e8dbe4c110f472b2d2afbe0d5de734a
BIGSosiska228 <andreymolofia@gmail.com>
2026-03-20 11:12:41 +0400

    Update line.h

--- line.h
+++ line.h
@@ -1,33 +1,58 @@
 #pragma once
 #include "Shape.h"
+#include <iostream>
+#include <cmath>
+#include <utility>
 
-class Line : public Shape
-{
+class Line : public Shape {
 private:
-    unsigned int width;
+    unsigned int x2;
+    unsigned int y2;
 
 public:
-    Line(unsigned int x, unsigned int y, unsigned int w)
-        : Shape(x, y)
-    {
-        width = w;
-    }
-
-    Line(const Line& source) : Shape(source)
-    {
-        width = source.width;
-    }
-
-    Shape* clone() override
-    {
+    Line(unsigned int x1, unsigned int y1, unsigned int x2, unsigned int y2) 
+        : Shape(x1, y1), x2(x2), y2(y2) {}
+    
+    Line(const Line& source) 
+        : Shape(source), x2(source.x2), y2(source.y2) {}
+    
+    Shape* clone() const override {
         return new Line(*this);
     }
-
-    void Draw() override
-    {
-        std::cout << "Line "
-            << x << " "
-            << y << " "
-            << width << std::endl;
+    
+    void Draw() const override {
+        std::cout << "Line from (" << x << "," << y 
+                  << ") to (" << x2 << "," << y2 << ")" << std::endl;
+    }
+    
+    void setSize(unsigned int size) override {
+        double dx = static_cast<double>(x2) - x;
+        double dy = static_cast<double>(y2) - y;
+        double currentLength = std::sqrt(dx*dx + dy*dy);
+        
+        if (currentLength > 0) {
+            double ratio = static_cast<double>(size) / currentLength;
+            x2 = x + static_cast<unsigned int>(dx * ratio);
+            y2 = y + static_cast<unsigned int>(dy * ratio);
+        }
+    }
+    
+    unsigned int getSize() const override {
+        return static_cast<unsigned int>(getLength());
+    }
+    
+    void setEndPoint(unsigned int x, unsigned int y) {
+        x2 = x;
+        y2 = y;
+    }
+    
+    std::pair<unsigned int, unsigned int> getEndPoint() const {
+        return {x2, y2};
+    }
+    
+    double getLength() const {
+        double dx = static_cast<double>(x2) - x;
+        double dy = static_cast<double>(y2) - y;
+        return std::sqrt(dx*dx + dy*dy);
     }
 };
